c858cd51395baeccd244d6e1cf0a129cc5d63a36
Carl Sutherland <carl@humblesoftware.com>
2012-08-10 17:01:57 -0400

    Initial solr backend prototype with fetch, query.

--- src/backend.solr.js
+++ src/backend.solr.js
@@ -22,6 +22,56 @@ this.recline.Backend.Solr = this.recline.Backend.Solr || {};
 
   };
 
+  my.fetch = function(dataset) {
+    var dfd = new $.Deferred();
+
+    my.query({
+      q : 'love',
+      size : 1,
+      from : 0
+    }, dataset).done(function (data) {
+      dfd.resolve({
+        fields : data.hits[0] ? _.keys(data.hits[0]) : []
+      });
+    });
+
+    return dfd;
+  },
+
+  my.query = function(query, dataset) {
+    var dfd = new $.Deferred();
+    var url = dataset.url + '/query';
+    var data = {
+      rows : query.size,
+      start : query.from,
+      q : query.q,
+      wt : 'json',
+      debugQuery : true
+    };
+
+    makeRequest({
+      url : url,
+      data : data,
+      dataType : 'jsonp',
+      jsonp : 'json.wrf'
+    }).done(function (data) {
+      var records = data.response.docs;
+      var total = data.response.numFound;
+
+      console.log(data);
+
+      dfd.resolve({
+        total : total,
+        hits : records
+      });
+
+    }).fail(function () {
+      dfd.reject(arguments);
+    });
+
+    return dfd;
+  };
+
 // ### makeReqest
 //
 // Factory for the xhr in case we ever need to modify its
